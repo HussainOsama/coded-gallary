98d999ef4a698d38a6903d66d2626e94166ae73d
HussainOsama <hussain.osama.asd@gmail.com>
2021-03-23 17:23:36 +0300

    new repository

---
+++ src/api/index.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+// const data = async () => {
+//   try {
+//     const data = await axios.get("http://localhost:5000/posts");
+//     return data;
+//   } catch (err) {
+//     console.log("Data Fetch Failed");
+//   }
+// };
+
+// let x = data();
+const url = "http://localhost:5000/posts";
+
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+export const updatePost = (id, updatedPost) =>
+  axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
+export const likePost = (id) => axios.patch(`${url}/${id}/likepost`);
+// router.patch("/posts/:id/likepost", likePost);
