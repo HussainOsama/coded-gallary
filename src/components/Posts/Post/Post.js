1f6cace9e62de92b510bb3de359f38fc64e735fc
HussainOsama <hussain.osama.asd@gmail.com>
2021-03-23 20:45:06 +0300

    add the Modal part

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -6,6 +6,7 @@ import {
   Button,
   Typography,
 } from "@material-ui/core";
+import React from "react";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
@@ -14,16 +15,79 @@ import useStyles from "./styles";
 import { useDispatch } from "react-redux";
 import { deletePost, likePost } from "../../../actions/posts";
 
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import { useSpring, animated } from "react-spring/web.cjs";
+
+const useMStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+  const { in: open, children, onEnter, onExited, ...other } = props;
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter();
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited();
+      }
+    },
+  });
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {children}
+    </animated.div>
+  );
+});
+
+Fade.propTypes = {
+  children: PropTypes.element,
+  in: PropTypes.bool.isRequired,
+  onEnter: PropTypes.func,
+  onExited: PropTypes.func,
+};
+
 function Post({ post, setCurrentId }) {
   const classes = useStyles();
+  const classesM = useMStyles();
+
   const dispatch = useDispatch();
 
   // const [CurrentId, setCurrentId] = useState(0);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
       <Card className={classes.card}>
         <CardMedia
+          onClick={handleOpen}
           className={classes.media}
           image={
             post.selectedFile ||
@@ -31,6 +95,30 @@ function Post({ post, setCurrentId }) {
           }
           title={post.title}
         />
+        <Modal
+          aria-labelledby="spring-modal-title"
+          aria-describedby="spring-modal-description"
+          className={classesM.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div className={classesM.paper}>
+              <img
+                height="500"
+                src={
+                  post.selectedFile ||
+                  "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
+                }
+              ></img>
+            </div>
+          </Fade>
+        </Modal>
         <div className={classes.overlay}>
           <Typography variant="h6">{post.creator}</Typography>
           <Typography variant="body2">
